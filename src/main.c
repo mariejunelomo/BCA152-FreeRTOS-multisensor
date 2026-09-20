9063341a65f1f9dbae3a8c5d264e0faa26701892
Marie Lomo <mariejune.lomo@g.msuiit.edu.ph>
2026-09-20 16:48:56 +0700

    Create initial FreeRTOS tasks

--- src/main.c
+++ src/main.c
@@ -1,8 +1,44 @@
-#include <esp_log.h>
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-const char *TAG = "MAIN";
+void taskA(void *pvParameters)
+{
+    while (1)
+    {
+        printf("Task A running\n");
 
-void app_main() {
-     ESP_LOGI(TAG, "BCA152 FreeRTOS Multisensor");
-     ESP_LOGI(TAG, "System starting...");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
+void taskB(void *pvParameters)
+{
+    while (1)
+    {
+        printf("Task B running\n");
+
+        vTaskDelay(pdMS_TO_TICKS(1500));
+    }
+}
+
+void app_main(void)
+{
+    xTaskCreate(
+        taskA,
+        "Task A",
+        2048,
+        NULL,
+        1,
+        NULL
+    );
+
+    xTaskCreate(
+        taskB,
+        "Task B",
+        2048,
+        NULL,
+        1,
+        NULL
+    );
 }
